5f22e831945f0ea26709636d8376520bb508b81f
dntam00 <dangngoctam00@gmail.com>
2025-07-12 23:24:31 +0700

    category module

--- src/main/java/loader/Loader.java
+++ src/main/java/loader/Loader.java
@@ -1,11 +1,31 @@
 package loader;
 
 import java.sql.DriverManager;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Loader {
 
     public static void main(String[] args) {
+//        printLoaderClass();
+//        categoryModule();
+    }
+
+    public static void categoryModule() {
+        ModuleLayer.boot().modules().stream()
+                   .collect(Collectors.groupingBy(
+                           m -> Optional.ofNullable(m.getClassLoader())
+                                        .map(ClassLoader::getName).orElse("boot"),
+                           Collectors.mapping(Module::getName,
+                                              Collectors.toCollection(TreeSet::new))))
+                   .entrySet().stream()
+                   .sorted(Comparator.comparingInt(
+                           e -> List.of("boot", "platform", "app").indexOf(e.getKey())))
+                   .map(e -> e.getKey() + "\n\t" + String.join("\n\t", e.getValue()))
+                   .forEach(System.out::println);
+    }
+
+    private static void printLoaderClass() {
         System.out.println("Classloader of ArrayList:"
                                    + ArrayList.class.getClassLoader());
         System.out.println("Classloader of DriverManager:"
@@ -13,4 +33,6 @@ public class Loader {
         System.out.println("Classloader of Loader:"
                                    + Loader.class.getClassLoader());
     }
+
+
 }
